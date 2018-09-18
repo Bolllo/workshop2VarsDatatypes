bca0da0d61c01d6145dc0b939717e7c2229a7d8e
ever0035 <ever0035@hz.nl>
2018-09-18 14:00:35 +0200

    commit

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+// Opdracht 1
+const waarde = (-9);
+let remainder = (waarde % 2);
+//Hier word berekend met een remainder operator of de waarde even of oneven is.
+
+console.log(`Ingevulde waarde is: ${waarde}`)
+if (remainder == 0 || remainder == -0) {
+    console.log("even")
+} else {
+    console.log("oneven")
+}
+// in een if else functie word gekeken of de remainder 0 of -0 is. is dat true dan is het getal even. zoniet dan is het getal oneven
+
+//Opdracht 2
+const message = 'Programming is not so cool'
+console.log(message)
+messagereplaced = message.replace ('not',"")
+console.log(messagereplaced)
+//ik vervang de not in de string met lege tekst. zodat de not uit de tekst wordt verwijderd.
+
+//opdracht 3
+const getal = (1400)
+const plaats = 'ik woon in Naboo'
+const vergelijking = (getal == plaats)
+console.log(vergelijking)
+//waarom zou je een number en string vergelijken? Het zijn twee compleet andere waardes.
+
+
